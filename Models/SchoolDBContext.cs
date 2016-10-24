8c8ef2dfa2922fa8f0345f244e05999441791a7a
Amir <shahinkar@gmail.com>
2016-10-24 15:30:03 -0500

    Uploud

---
+++ Models/SchoolDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace School.Models
+{
+    public class SchoolDBContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+
+}
---
+++ Scripts/App/controller.js
@@ -0,0 +1,64 @@
+﻿var app = angular.module("MyApp", ["ngResource", "ui.router"]);
+
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+        .state("list", {
+            url: "/list",
+            templateUrl: "/ngView/list.html",
+            controller: "listCtrl"
+        })
+        .state("list.detail", {
+            url: "/detail/{id}",
+            templateUrl: "/ngView/detail.html",
+            controller: "detailCtrl"
+        })
+        .state("list.detail.form", {
+            url: "/form",
+            templateUrl: "/ngView/form.html",
+            controller: "formCtrl"
+        });
+
+    // For any unmatched url, redirect to /list
+    $urlRouterProvider.otherwise("/list");
+
+});
+
+app.controller("listCtrl", function ($scope, APIservice) {
+
+    APIservice.query(function (Data) {
+        $scope.dataList = Data;
+    });
+
+    $scope.edit = function (id) {
+        $location.path("/form/" + (id));
+    }
+
+});
+
+app.controller("detailCtrl", function ($scope, $stateParams, APIservice) {
+
+    $scope.dataDetail = APIservice.get({ id: $stateParams.id });
+
+});
+
+app.controller("formCtrl", function ($scope, $state, $stateParams, APIservice) {
+
+    if ($stateParams.id)
+        $scope.user = APIservice.get({ id: $stateParams.id });
+
+    $scope.submit = function (user) {
+        APIservice.update({ id: $stateParams.id }, user, function () {
+            $state.go("list", {}, { reload: true });
+        });
+    }
+});
+
+app.service("APIservice", function ($resource) {
+
+    return $resource("/api/Students/:id", null, { "update": { method: "PUT" } });
+
+});
+
